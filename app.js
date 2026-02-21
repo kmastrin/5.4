f3c49f31a4bb51ed9aac63271806d90ad087495a
kmastrin <92259561+kmastrin@users.noreply.github.com>
2026-02-20 22:08:12 -0600

    index

---
+++ app.js
@@ -0,0 +1,23 @@
+const text = document.querySelector(".title");
+const changeColor = document.querySelector(".changeColor");
+
+text.style.color = "ffffff"; // change CSS with JS
+
+changeColor.addEventListener("click", function() {
+    text.classList.toggle("change");
+});
+
+const userList = document.querySelectorAll(".inventory li");
+console.log(userList);
+
+for(user of userList) {
+    user.addEventListener("click", function() {
+        this.style.textDecoration = "line-through";
+    });
+} // For Loop
+
+let i = 1;
+while (i < 8) {
+  console.log('Count:', i);
+  i++;
+} // While Loop
